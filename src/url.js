671ad5d7fd46aaa80f7144b1ef4642d8334078f1
Nithish Kumar <nithishlearning45@gmail.com>
2024-06-22 21:24:54 +0530

    redeployed to render

--- src/url.js
+++ src/url.js
@@ -1,2 +1,4 @@
-export const baseurl='http://54.161.199.13/api'
+export const baseurl='https://backend-deployment-2-31hq.onrender.com/api'
 //https://backend-deployment-1-tqk1.onrender.com
+
+// http://54.161.199.13 -- aws domains
